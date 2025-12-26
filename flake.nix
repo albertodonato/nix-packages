8edff682ccbd1e490193f849d4378bbe5d97d2ab
Alberto Donato <git@albertodonato.net>
2025-12-26 10:58:41 +0100

    chore: default nixpkgs url to unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Personal Nix packages repository";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs =
